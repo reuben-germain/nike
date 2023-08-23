ff8cf1a096a1b425924b6fee67fe63c4d12b13a2
Reuben Germain <reuben.germain@soprasteria.com>
2023-08-23 13:45:59 +0100

    added a Nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality } from "./sections";
+import Nav from './components/Nav';
 
 const App = () => (
   <main className="relative">
-    {/* <Nav/> */}
+    <Nav/>
     <section className="xl:padding-1 wide:padding-r padding-b">
       <Hero/>
     </section>
