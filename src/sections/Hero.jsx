0c2baa3acb5501e7b99635aaa0524a671a1db38e
Reuben Germain <reuben.germain@soprasteria.com>
2023-08-30 10:02:37 +0100

    started on my hero.jsx file, added some intial tailwind classes with a border to see what is going on

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,8 @@
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container border-2 border-red-500 p-2">
+
+    </section>
   )
 }
 
