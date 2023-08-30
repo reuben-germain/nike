cbc4a883cc8e3bc80eaf919e9817910395584836
Reuben Germain <reuben.germain@soprasteria.com>
2023-08-30 10:11:42 +0100

    added a div with tailwind classes inside my section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,9 @@
 const Hero = () => {
   return (
     <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
 
+      </div>
     </section>
   )
 }
