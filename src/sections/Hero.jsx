0baff53266dd65d5fb3beb4ce619587e820de778
Reuben Germain <reuben.germain@soprasteria.com>
2023-08-30 10:58:01 +0100

    added the stats text below the button

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,6 @@
 import Button from "../components/Button";
 import {arrowRight} from '../assets/icons'
+import { statistics } from '../constants'
 
 const Hero = () => {
   return (
@@ -9,6 +10,14 @@ const Hero = () => {
         <h1><span>The New Arrival</span><br /><span>Nike</span>Shoes</h1>
         <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
         <Button label="Shop Now" iconURL={arrowRight}></Button>
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+          {statistics.map((stat, index) => (<div>
+            <p>{stat.value}</p>
+            <p>{stat.label}</p>
+          </div>
+          ))}
+
+        </div>
       </div>
     </section>
   )
