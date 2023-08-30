3513ada4f85421f0118ec83493d9396ecd0f8141
Reuben Germain <reuben.germain@soprasteria.com>
2023-08-30 10:04:02 +0100

    removed border & padding

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container border-2 border-red-500 p-2">
+    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
 
     </section>
   )
