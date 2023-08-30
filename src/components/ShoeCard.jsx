778c61fede80c927aece9dffa0d364e55fe53bc8
Reuben Germain <reuben.germain@soprasteria.com>
2023-08-30 16:58:11 +0100

    added some logic to change shoe images. Must fix the way the shoe image is showing at some point. It's suppose to be at the side of the text that says 'The New Arrival

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,6 +1,22 @@
-const ShoeCard = () => {
+const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
+
+    const handleClick = () => {
+        if(bigShoeImg !== imgURL.bigShoe)
+        {
+            changeBigShoeImage(imgURL.bigShoe)
+        }
+    }
   return (
-    <div>ShoeCard</div>
+    <div className={`border-2 
+    rounded-xl
+    ${bigShoeImg === imgURL
+    ? 'border-coral-red'
+    : 'border-transparent'
+    } cursor-pointer max-sm:flex-1
+`}
+onClick={handleClick}>
+
+    </div>
   )
 }
 
