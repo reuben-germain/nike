a34f8fbbf9958e4d54b57536c26a1782e160ff6f
Reuben Germain <reuben.germain@soprasteria.com>
2023-08-31 22:45:37 +0100

    made some changes so that when any of the three shoes are clicked, you can see the border around the selected option

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -8,7 +8,7 @@ const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
     return (
         <div className={`border-2 
     rounded-xl
-    ${bigShoeImg === imgURL
+    ${bigShoeImg === imgURL.bigShoe
                 ? 'border-coral-red'
                 : 'border-transparent'
             } cursor-pointer max-sm:flex-1
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,10 +1,12 @@
 import Button from "../components/Button";
+import { useState } from "react";
 import {arrowRight} from '../assets/icons'
 import { shoes, statistics } from '../constants'
 import { bigShoe1 } from '../assets/images';
 import ShoeCard from "../components/ShoeCard";
 
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1)
   return (
     <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
@@ -22,13 +24,13 @@ const Hero = () => {
       </div>
 
       <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
-        <img src={bigShoe1} alt="shoe collection" width={610} height={500} className="object-contain relative z-10" />
+        <img src={bigShoeImg} alt="shoe collection" width={610} height={500} className="object-contain relative z-10" />
 
         <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
           {shoes.map((shoe) =>
           (
             <div key={shoe}>
-              <ShoeCard imgURL={shoe} changeBigShoeImage={() => {}} bigShoeImg=""/>
+              <ShoeCard imgURL={shoe} changeBigShoeImage={(shoe) => setBigShoeImg(shoe)} bigShoeImg={bigShoeImg}/>
             </div>
           ))}
         </div>
