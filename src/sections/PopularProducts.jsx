b205a62e0e111afdd461a7feaffe9282b420ebdb
Reuben Germain <reuben.germain@soprasteria.com>
2023-09-05 14:00:13 +0100

    finished shoe card & started on popular products

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,6 +1,15 @@
+import { products } from "../constants";
+
 const PopularProducts = () => {
   return (
-    <div>PopularProducts</div>
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2>Popular Products</h2>
+        <p>Experience top-notch quality and style with our sought-after selections. Discover a world of comfort, design, and value</p>
+
+      </div>
+
+    </section>
   )
 }
 
