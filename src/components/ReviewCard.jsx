2604e85ce3d9e2db66d3d291b29f479cb3b7a8ef
Reuben Germain <reuben.germain@soprasteria.com>
2023-09-06 17:48:37 +0100

    finished off review card

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -5,11 +5,11 @@ const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
     <div className="flex justify-center items-center flex-col">
         <img src={imgURL} alt="customer" className="rounded-full object-cover w-[120px] h-[120px]" />
         <p>{feedback}</p>
-        <div>
+        <div className="mt-3 flex justify-center items-center gap-2.5">
             <img src={star} width={24} height={24} className="object-contain m-0" />
-            <p>({rating})</p>
+            <p className="text-xl font-montserrat text-slate-gray">({rating})</p>
         </div>
-        <h3>{customerName}</h3>
+        <h3 className="mt-1 font-palanquin text-3xl text-center font-bold">{customerName}</h3>
 
     </div>
   )
