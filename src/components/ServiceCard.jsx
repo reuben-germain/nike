1f8665b7a8a698c0d87f16b7de1c8e6352b2a2ae
Reuben Germain <reuben.germain@soprasteria.com>
2023-09-06 12:08:33 +0100

    started to style service cards

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,6 +1,11 @@
-const ServiceCard = () => {
+const ServiceCard = ({ imgURL, label, subtext }) => {
   return (
-    <div>ServiceCard</div>
+  <div className="flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16">
+    <div className="w-11 h-11 flex justify-center items-center bg-coral-red rounded-4">
+        <img src={imgURL} alt={label} width={24} height={24} />
+    </div>
+
+  </div>
   )
 }
 
