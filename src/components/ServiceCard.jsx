17af22d9d782d92cc2c0df74151dc9b62c4e0026
Reuben Germain <reuben.germain@soprasteria.com>
2023-09-06 12:13:18 +0100

    added classnames to the h3 & p tag

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -4,6 +4,8 @@ const ServiceCard = ({ imgURL, label, subtext }) => {
     <div className="w-11 h-11 flex justify-center items-center bg-coral-red rounded-4">
         <img src={imgURL} alt={label} width={24} height={24} />
     </div>
+    <h3 className="mt-5 font-palanquin text-3xl leading-normal font-bold">{label}</h3>
+    <p className="mt-3 break-words font-montserrat text-lg leading-normal text-slate-gray">{subtext}</p>
 
   </div>
   )
