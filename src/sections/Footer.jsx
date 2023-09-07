afe94e85eb855bd3ef0be2eed5078ce2dae23e67
Reuben Germain <reuben.germain@soprasteria.com>
2023-09-07 12:45:38 +0100

    FINISHED

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,6 +1,8 @@
-import { copyrightSign } from "../assets/icons"
-import { footerLogo } from "../assets/images"
-import { footerLinks, socialMedia } from "../constants"
+import React from "react"; // Make sure to import React if you're using JSX.
+
+import { copyrightSign } from "../assets/icons";
+import { footerLogo } from "../assets/images";
+import { footerLinks, socialMedia } from "../constants";
 
 const Footer = () => {
   return (
@@ -8,48 +10,47 @@ const Footer = () => {
       <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
         <div className="flex flex-col items-start">
           <a href="/">
-<img src={footerLogo} width={150} height={46} alt="" />            
+            <img src={footerLogo} width={150} height={46} alt="" />
           </a>
-          <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">Get shoes ready for the new term at your nearest Nike store. Find Your Perfect Size In Store. Get Rewards</p>
+          <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">
+            Get shoes ready for the new term at your nearest Nike store. Find Your Perfect Size In Store. Get Rewards
+          </p>
           <div className="flex items-center gap-5 mt-8">
-            {socialMedia.map((icon) => (
-              <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full">
+            {socialMedia.map((icon, index) => (
+              <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full" key={index}>
                 <img src={icon.src} alt={icon.alt} width={24} height={24} />
               </div>
-
             ))}
-
           </div>
-
         </div>
+        
         <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
           {footerLinks.map((section) => (
-            <div key={section}>
+            <div key={section.title}>
               <h4 className="text-white font-montserrat text-2xl leading-normal font-medium mb-6">{section.title}</h4>
               <ul>
-                {section.links.map((link) => (
-                  <li className="mt-3 text-white-400 font-montserrat text-base leading-normal hover:text-slate-gray cursor-pointer"
-                  key={link.name}>
-                    <a>{link.name}</a>
+                {section.links.map((link, index) => (
+                  <li
+                    className="mt-3 text-white-400 font-montserrat text-base leading-normal hover:text-slate-gray cursor-pointer"
+                    key={index} // Use a unique key, you can use the array index if link.name isn't unique.
+                  >
+                    <a href="#">{link.name}</a>
                   </li>
                 ))}
               </ul>
-
             </div>
           ))}
         </div>
       </div>
-
       <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
         <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
           <img src={copyrightSign} alt="copy right sign" width={20} height={20} className="rounded-full m-0" />
-          <p>Copyright. All Rights Reservered.</p>
+          <p>Copyright. All Rights Reserved.</p> {/* Fixed typo in "Reserved". */}
         </div>
         <p className="font-montserrat cursor-pointer">Terms & Conditions</p>
-
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
