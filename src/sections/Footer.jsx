0ecba1a25418b1f193018ef136e7657340cade9f
Reuben Germain <reuben.germain@soprasteria.com>
2023-09-07 11:53:49 +0100

    started footer section

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,6 +1,18 @@
+import { footerLogo } from "../assets/images"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="max-container">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+        <div className="flex flex-col items-start">
+          <a href="/">
+<img src={footerLogo} width={150} height={46} alt="" />            
+          </a>
+          <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">Get shoes ready for the new term at your nearest Nike store. Find Your Perfect Size In Store. Get Rewards</p>
+
+        </div>
+      </div>
+    </footer>
   )
 }
 
