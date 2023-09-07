b70f6a4b81a601ad302d461c719cf720d5c52c08
Reuben Germain <reuben.germain@soprasteria.com>
2023-09-07 11:10:44 +0100

    trying to fix clickable images

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -7,6 +7,11 @@ import ShoeCard from "../components/ShoeCard";
 
 const Hero = () => {
   const [bigShoeImg, setBigShoeImg] = useState(bigShoe1)
+
+  // Define a function to change the big shoe image
+  const changeBigShoeImage = (shoe) => {
+    setBigShoeImg(shoe);
+  };
   return (
     <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
