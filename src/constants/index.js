2ca26ee824ab75597ed0dfee9dabb5501d69e37b
Reuben Germain <reuben.germain@soprasteria.com>
2023-09-07 10:48:08 +0100

    finished subscribe section

--- src/constants/index.js
+++ src/constants/index.js
@@ -33,22 +33,22 @@ export const products = [
     {
         imgURL: shoe4,
         name: "Nike Air Jordan-01",
-        price: "$200.20",
+        price: "£200.20",
     },
     {
         imgURL: shoe5,
         name: "Nike Air Jordan-10",
-        price: "$210.20",
+        price: "£210.20",
     },
     {
         imgURL: shoe6,
         name: "Nike Air Jordan-100",
-        price: "$220.20",
+        price: "£220.20",
     },
     {
         imgURL: shoe7,
         name: "Nike Air Jordan-001",
-        price: "$230.20",
+        price: "£230.20",
     },
 ];
 
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -3,7 +3,7 @@ import { shoe8 } from "../assets/images"
 const SuperQuality = () => {
   return (
     <section id="about-us"
-    className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container">
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container">
       <div className="flex flex-1 flex-col">
 
         <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
@@ -13,11 +13,11 @@ const SuperQuality = () => {
           <span className="text-coral-red">
             Quality
           </span> Shoes</h2>
-          <p className="mt-4 lg:max-w-lg info-text">Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.</p>
-          <p className="mt-6 lg:max-w-lg info-text">Our dedication to detail and excellence ensures your satisfaction</p>
-          <div className="mt-11 ">
+        <p className="mt-4 lg:max-w-lg info-text">Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.</p>
+        <p className="mt-6 lg:max-w-lg info-text">Our dedication to detail and excellence ensures your satisfaction</p>
+        <div className="mt-11 ">
           <Button label="View details"></Button>
-          </div>
+        </div>
       </div>
 
       <div className="flex-1 flex justify-center items-center">
